3996b94d1fe70d1f1c9f452ec5b74f565cc05af2
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-17 12:15:07 +0100

    scheduled functions to fetch the data

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const app = require('./src/app');
 const port = process.env.PORT || 8080;
+<<<<<<< Updated upstream
+=======
+const createError = require('http-errors');
+const scheduledFunctions = require('./scheduledFunctions')
+>>>>>>> Stashed changes
 
 app.listen(port, () => {
   console.log(`RAIDDON listening on ${port}!`);
 });
+
+// Call to execute scheduled function(s)
+scheduledFunctions.initScheduledJobs();
 
 //Generating error page
 app.use(async (res, req, next) => {
