5dea052553c8c6b26d55425fb7f5571c5d035489
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-18 20:01:23 +0100

    removing blizzapi

--- src/routes/app/utils/index.js
+++ src/routes/app/utils/index.js
@@ -1,67 +1,67 @@
-const router = require('express').Router();
-const { BlizzAPI } = require('blizzapi');
+// const router = require('express').Router();
+// const { BlizzAPI } = require('blizzapi');
 
-/** Regions */
-router.get('/getAllRegions', (req, res) => {
-  res.json(BlizzAPI.getAllRegions());
-});
+// /** Regions */
+// router.get('/getAllRegions', (req, res) => {
+//   res.json(BlizzAPI.getAllRegions());
+// });
 
-router.get('/getAllRegionIds', (req, res) => {
-  res.json(BlizzAPI.getAllRegionIds());
-});
+// router.get('/getAllRegionIds', (req, res) => {
+//   res.json(BlizzAPI.getAllRegionIds());
+// });
 
-router.get('/getAllRegionNames', (req, res) => {
-  res.json(BlizzAPI.getAllRegionNames());
-});
+// router.get('/getAllRegionNames', (req, res) => {
+//   res.json(BlizzAPI.getAllRegionNames());
+// });
 
-router.get('/getRegionNameById/:regionId', (req, res) => {
-  const { regionId } = req.params;
-  res.json(BlizzAPI.getRegionNameById(regionId));
-});
+// router.get('/getRegionNameById/:regionId', (req, res) => {
+//   const { regionId } = req.params;
+//   res.json(BlizzAPI.getRegionNameById(regionId));
+// });
 
-router.get('/getRegionIdByName/:regionName', (req, res) => {
-  const { regionName } = req.params;
-  res.json(BlizzAPI.getRegionIdByName(regionName));
-});
+// router.get('/getRegionIdByName/:regionName', (req, res) => {
+//   const { regionName } = req.params;
+//   res.json(BlizzAPI.getRegionIdByName(regionName));
+// });
 
-router.get('/validateRegionId/:regionId', (req, res) => {
-  const { regionId } = req.params;
-  res.json(BlizzAPI.validateRegionId(regionId));
-});
+// router.get('/validateRegionId/:regionId', (req, res) => {
+//   const { regionId } = req.params;
+//   res.json(BlizzAPI.validateRegionId(regionId));
+// });
 
-router.get('/validateRegionName/:regionName', (req, res) => {
-  const { regionName } = req.params;
-  res.json(BlizzAPI.validateRegionName(regionName));
-});
+// router.get('/validateRegionName/:regionName', (req, res) => {
+//   const { regionName } = req.params;
+//   res.json(BlizzAPI.validateRegionName(regionName));
+// });
 
-/** Locales */
+// /** Locales */
 
-router.get('/getAllLocales', (req, res) => {
-  res.json(BlizzAPI.getAllLocales());
-});
+// router.get('/getAllLocales', (req, res) => {
+//   res.json(BlizzAPI.getAllLocales());
+// });
 
-router.get('/getAllLocaleNames', (req, res) => {
-  res.json(BlizzAPI.getAllLocaleNames());
-});
+// router.get('/getAllLocaleNames', (req, res) => {
+//   res.json(BlizzAPI.getAllLocaleNames());
+// });
 
-router.get('/getLocalesByRegionId/:regionId', (req, res) => {
-  const { regionId } = req.params;
-  res.json(BlizzAPI.getLocalesByRegionId(regionId));
-});
+// router.get('/getLocalesByRegionId/:regionId', (req, res) => {
+//   const { regionId } = req.params;
+//   res.json(BlizzAPI.getLocalesByRegionId(regionId));
+// });
 
-router.get('/checkIfLocaleLooksValid/:locale', (req, res) => {
-  const { locale } = req.params;
-  res.json(BlizzAPI.checkIfLocaleLooksValid(locale));
-});
+// router.get('/checkIfLocaleLooksValid/:locale', (req, res) => {
+//   const { locale } = req.params;
+//   res.json(BlizzAPI.checkIfLocaleLooksValid(locale));
+// });
 
-router.get('/validateLocale/:locale', (req, res) => {
-  const { locale } = req.params;
-  res.json(BlizzAPI.validateLocale(locale));
-});
+// router.get('/validateLocale/:locale', (req, res) => {
+//   const { locale } = req.params;
+//   res.json(BlizzAPI.validateLocale(locale));
+// });
 
-router.get('/isLocaleValidForRegionId/:locale/:regionId', (req, res) => {
-  const { locale, regionId } = req.params;
-  res.json(BlizzAPI.isLocaleValidForRegionId(locale, regionId));
-});
+// router.get('/isLocaleValidForRegionId/:locale/:regionId', (req, res) => {
+//   const { locale, regionId } = req.params;
+//   res.json(BlizzAPI.isLocaleValidForRegionId(locale, regionId));
+// });
 
-module.exports = router;
+// module.exports = router;
