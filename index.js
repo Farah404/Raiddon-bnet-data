5548b2b0aa1a61af8e5c8a339ab24e0f2720e287
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-22 20:40:19 +0100

    find methods for db

--- index.js
+++ index.js
@@ -1,25 +1,396 @@
-require('dotenv').config();
-const app = require('./src/app');
-const createError = require('http-errors');
-const scheduledFunctions = require('./scheduledFunctions')
+require("dotenv").config();
+const app = require("./src/app");
+const scheduledFunctions = require("./scheduledFunctions");
+var MongoClient = require("mongodb").MongoClient;
+const { env } = process;
+const uri = env.MONGO_DB_URI;
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+MongoClient.connect(uri, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("raiddon-bnet-api");
+
+  dbo
+    .collection("areas")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var areas = result;
+    });
+
+  dbo
+    .collection("achievements")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var achievements = result;
+    });
+
+  dbo
+    .collection("armor")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var armor = result;
+    });
+
+  dbo
+    .collection("auction-house-index")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var auctionHouseIndex = result;
+    });
+
+  dbo
+    .collection("character-specializations")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var characSpec = result;
+    });
+
+  dbo
+    .collection("class-talents")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var classTalents = result;
+    });
+
+  dbo
+    .collection("consumables")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var consumables = result;
+    });
+
+  dbo
+    .collection("containers")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var containers = result;
+    });
+
+  dbo
+    .collection("creature-families")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var creatureFamilies = result;
+    });
+
+  dbo
+    .collection("creature-types")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var creatureTypes = result;
+    });
+
+  dbo
+    .collection("gems")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var gems = result;
+    });
+
+  dbo
+    .collection("item-classes")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var itemClasses = result;
+    });
+
+  dbo
+    .collection("mounts")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var mounts = result;
+    });
+
+  dbo
+    .collection("playable-classes")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var playableClasses = result;
+    });
+
+  dbo
+    .collection("playable-races")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var playableRaces = result;
+    });
+
+  dbo
+    .collection("power-types")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var powertypes = result;
+    });
+
+  dbo
+    .collection("professions")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var professions = result;
+    });
+
+  dbo
+    .collection("quest-categories")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var questCategories = result;
+    });
+
+  dbo
+    .collection("realms")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var realms = result;
+    });
+
+  dbo
+    .collection("reputation-factions")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var reputationFactions = result;
+    });
+
+  dbo
+    .collection("titles")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var titles = result;
+    });
+
+  dbo
+    .collection("weapons")
+    .find(
+      {},
+      {
+        projection: {
+          _id: false,
+          name: true,
+          id: true,
+        },
+      }
+    )
+    .toArray(function (err, result) {
+      if (err) throw err;
+      var weapons = result;
+    });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 // Call to execute scheduled function(s)
 scheduledFunctions.initScheduledJobs();
-
-//Generating error page
-app.use(async (res, req, next) => {
-  next(createError.NotFound())
-})
-app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send({
-      error: {
-          status: err.status ||500,
-          message: err.message
-      }
-  })
-})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
   app.use('/', require('./app'));
+  // app.use('/crud', require('./app/crud/routes'))
   // app.use('/api', require('./app/api'));
   // app.use('/utils', require('./app/utils'));
 };
