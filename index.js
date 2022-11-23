ed7aaae6ef5de6738a314e8eab197312c8fc9b92
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-23 19:11:13 +0100

    removed gems

--- index.js
+++ index.js
@@ -43,6 +43,18 @@ dbo.connectToServer(function (err) {
   });
 });
 
+//CORS middleware
+var corsMiddleware = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); //replace localhost with actual host
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+
+  next();
+  
+}
+app.use(corsMiddleware);
+
+
 
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -29,21 +29,5 @@ module.exports = (app) => {
  
   app.use('*',function (req, res) {
     res.redirect('/');
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    
-        // Pass to next layer of middleware
-        next();
-
 });
 };
