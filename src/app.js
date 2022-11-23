14b6fe071bac9749550457a116dbc12b381c048e
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-23 18:49:06 +0100

    removed gems

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const cors = require("cors");
 const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var corsOptions = {
-    origin: "http://localhost:8085"
-  };
-  app.use(cors(corsOptions));
+
   // parse requests of content-type - application/json
   app.use(express.json());
   // parse requests of content-type - application/x-www-form-urlencoded
